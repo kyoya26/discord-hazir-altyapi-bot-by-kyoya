da38650ee5c10f249cbb49399cbe64344a50776f
Kyoya <58367199+kyoya26@users.noreply.github.com>
2020-02-09 23:05:08 +0300

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,10 @@
+const ayarlar = require('../ayarlar.json');
+const dc = require('discord.js');
+
+module.exports = async (client) => {
+
+
+client.user.setActivity('Kyoya was here my dudeeeee') //XDDDDDD
+client.user.setStatus('dnd')
+
+}
